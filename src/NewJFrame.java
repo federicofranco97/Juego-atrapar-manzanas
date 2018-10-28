6d4c54715f6ebdf2f0f13c62c086cac0dc9c07c7
Fede <federicoa.franco@comunidad.ub.edu.ar>
2018-10-28 20:21:28 -0300

    create

--- src/NewJFrame.java
+++ src/NewJFrame.java
@@ -8,30 +8,65 @@ public class NewJFrame extends javax.swing.JFrame {
 
     public NewJFrame() {
         initComponents();
+        setLocationRelativeTo(null);
     }
     
-    static int i=0;
+    static int i;
+    static int i2;
+    static int i3;
+    
     
     
     
     public void generar(){
-        int x=lbl1.getX();
+        int x=lbl1.getX();     
         int y=lbl1.getY()+1;
         lbl1.setLocation(x, y);
+        int y2=lbl2.getY()+1;
+        int x2=lbl2.getX();
+        lbl2.setLocation(x2, y2);
+        int x3=lbl3.getX(); 
+        int y3=lbl3.getY()+1;
+        lbl3.setLocation(x3, y3);
+        
+       
     }
+    
+    public void generar2(){
+        Timer timer = new Timer(15,null);
+        
+        int y2=lbl2.getY()+1;
+        int x2=lbl2.getX();
+        lbl2.setLocation(x2, y2);
+    }
+    
+    public void generar3(){
+        int x3=lbl3.getX(); 
+        int y3=lbl3.getY()+1;
+        lbl3.setLocation(x3, y3);
+    }
+    
+  
     
     public void traer(){
        
-        
-        Timer timer = new Timer(50,null);
+        i=0;
+        Timer timer = new Timer(15,null);
         timer.setRepeats(true);
         timer.addActionListener((e)->
         {
             generar();
             i++;
-            if(i==100){
+            i2++;
+            i3++;
+            
+            if(i==150){
                 timer.stop();
-                lbl1.setVisible(false);
+                
+                lbl1.setLocation( lbl1.getX(), 0);
+                lbl2.setLocation( lbl2.getX(), 0);
+                lbl3.setLocation( lbl3.getX(), 0);
+                traer();
             }
         
         });
@@ -48,37 +83,67 @@ public class NewJFrame extends javax.swing.JFrame {
 
         lbl1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jSeparator1 = new javax.swing.JSeparator();
+        lbl2 = new javax.swing.JLabel();
+        lbl3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        lbl1.setBackground(new java.awt.Color(153, 255, 153));
         lbl1.setText("/ (0_0) /");
+        lbl1.setOpaque(true);
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Start");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
 
+        lbl2.setBackground(new java.awt.Color(153, 255, 153));
+        lbl2.setText("/ (0_0) /");
+        lbl2.setOpaque(true);
+
+        lbl3.setBackground(new java.awt.Color(153, 255, 153));
+        lbl3.setText("/ (0_0) /");
+        lbl3.setOpaque(true);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(271, 271, 271)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lbl1)
-                    .addComponent(jButton1))
-                .addContainerGap(466, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(273, 273, 273)
+                        .addComponent(jButton1)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jSeparator1)))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(151, 151, 151)
+                .addComponent(lbl3)
+                .addGap(94, 94, 94)
+                .addComponent(lbl1)
+                .addGap(90, 90, 90)
+                .addComponent(lbl2)
+                .addContainerGap(356, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(124, 124, 124)
-                .addComponent(lbl1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 159, Short.MAX_VALUE)
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbl1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lbl2, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lbl3, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(144, 144, 144)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 131, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addGap(80, 80, 80))
+                .addGap(72, 72, 72))
         );
 
         pack();
@@ -125,6 +190,9 @@ public class NewJFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JLabel lbl1;
+    private javax.swing.JLabel lbl2;
+    private javax.swing.JLabel lbl3;
     // End of variables declaration//GEN-END:variables
 }
