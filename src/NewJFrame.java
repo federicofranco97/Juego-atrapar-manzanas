953bca164c24125e58e922425322c530bf4a85fe
federico franco- note <43964230+federicofranco97@users.noreply.github.com>
2018-10-31 19:44:30 -0300

    mods

--- src/NewJFrame.java
+++ src/NewJFrame.java
@@ -1,12 +1,15 @@
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javafx.scene.Cursor;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
 public class NewJFrame extends javax.swing.JFrame {
 
     public NewJFrame() {
+        tiros=0;
         initComponents();
         setLocationRelativeTo(null);
     }
@@ -14,6 +17,7 @@ public class NewJFrame extends javax.swing.JFrame {
     static int i;
     static int i2;
     static int i3;
+    static int tiros;
     
     
     
@@ -77,81 +81,134 @@ public class NewJFrame extends javax.swing.JFrame {
         
     }
     
+    public void checkTiros(){
+        if(tiros == 3){
+            
+            lbl1.setVisible(false);
+            lbl2.setVisible(false);
+            lbl3.setVisible(false);
+            JOptionPane.showMessageDialog(null, "Ganaste!");
+        }
+    }
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lbl1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jSeparator1 = new javax.swing.JSeparator();
-        lbl2 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        lbl1 = new javax.swing.JLabel();
         lbl3 = new javax.swing.JLabel();
+        lbl2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(814, 426));
+        getContentPane().setLayout(null);
 
-        lbl1.setBackground(new java.awt.Color(153, 255, 153));
-        lbl1.setText("/ (0_0) /");
-        lbl1.setOpaque(true);
-
+        jButton1.setBackground(new java.awt.Color(255, 255, 255));
         jButton1.setText("Start");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(273, 322, 250, 32);
+        getContentPane().add(jSeparator1);
+        jSeparator1.setBounds(6, 165, 802, 10);
 
-        lbl2.setBackground(new java.awt.Color(153, 255, 153));
-        lbl2.setText("/ (0_0) /");
-        lbl2.setOpaque(true);
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
-        lbl3.setBackground(new java.awt.Color(153, 255, 153));
-        lbl3.setText("/ (0_0) /");
+        lbl1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/kisspng-apple-food-fruit-drawing-odisea-the-game-manzana-png-5b4acdd6706fd1.6360847015316290144606.jpg"))); // NOI18N
+        lbl1.setEnabled(false);
+        lbl1.setOpaque(true);
+        lbl1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lbl1MouseClicked(evt);
+            }
+        });
+
+        lbl3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/kisspng-apple-food-fruit-drawing-odisea-the-game-manzana-png-5b4acdd6706fd1.6360847015316290144606.jpg"))); // NOI18N
+        lbl3.setEnabled(false);
         lbl3.setOpaque(true);
+        lbl3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lbl3MouseClicked(evt);
+            }
+        });
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(273, 273, 273)
-                        .addComponent(jButton1)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jSeparator1)))
-                .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addGap(151, 151, 151)
-                .addComponent(lbl3)
-                .addGap(94, 94, 94)
+        lbl2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/kisspng-apple-food-fruit-drawing-odisea-the-game-manzana-png-5b4acdd6706fd1.6360847015316290144606.jpg"))); // NOI18N
+        lbl2.setEnabled(false);
+        lbl2.setOpaque(true);
+        lbl2.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                lbl2FocusGained(evt);
+            }
+        });
+        lbl2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lbl2MouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(156, 156, 156)
                 .addComponent(lbl1)
-                .addGap(90, 90, 90)
+                .addGap(172, 172, 172)
                 .addComponent(lbl2)
-                .addContainerGap(356, Short.MAX_VALUE))
+                .addGap(165, 165, 165)
+                .addComponent(lbl3)
+                .addContainerGap(246, Short.MAX_VALUE))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbl1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lbl2, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lbl3, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(144, 144, 144)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 131, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(72, 72, 72))
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lbl3)
+                    .addComponent(lbl2)
+                    .addComponent(lbl1))
+                .addGap(0, 415, Short.MAX_VALUE))
         );
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 0, 820, 430);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        lbl1.setEnabled(true);
+        lbl2.setEnabled(true);
+        lbl3.setEnabled(true);
         traer();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void lbl1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbl1MouseClicked
+        lbl1.setVisible(false);
+        tiros++;
+        checkTiros();
+    }//GEN-LAST:event_lbl1MouseClicked
+
+    private void lbl3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbl3MouseClicked
+        lbl2.setVisible(false);
+        tiros++;
+        checkTiros();
+    }//GEN-LAST:event_lbl3MouseClicked
+
+    private void lbl2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbl2MouseClicked
+        lbl3.setVisible(false);
+        tiros++;
+        checkTiros();
+    }//GEN-LAST:event_lbl2MouseClicked
+
+    private void lbl2FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_lbl2FocusGained
+        
+    }//GEN-LAST:event_lbl2FocusGained
 
     /**
      * @param args the command line arguments
@@ -190,6 +247,7 @@ public class NewJFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JLabel lbl1;
     private javax.swing.JLabel lbl2;
